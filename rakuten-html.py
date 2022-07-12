983018b7c5eda97a4c1f52a13fd9e4dff6904a82
kuwamoto <y_kuwamoto@webkikaku.co.jp>
2022-07-12 16:23:03 +0900

    /Users/y-001/Documents/GitHub/ecsiteseo001/runtime.txt

---
+++ rakuten-html.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+import chromedriver_binary
+
+driver_path = "/app/.chromedriver/bin/chromedriver"
+driver = webdriver.Chrome(executable_path=driver_path)
+
+driver.get('https://search.rakuten.co.jp/search/mall/%E3%83%80%E3%82%A4%E3%82%A8%E3%83%83%E3%83%88/')
+
+html = driver.page_source
