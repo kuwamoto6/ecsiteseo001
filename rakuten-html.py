c3ebbf2c2143c1f4a8df0302a3ce2ff23e66931d
kuwamoto6 <y_kuwamoto@webkikaku.co.jp>
2022-07-13 16:42:04 +0900

    Update rakuten-html.py

--- rakuten-html.py
+++ rakuten-html.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import chromedriver_binary
 
-driver_path = "/app/.chromedriver/bin/chromedriver"
-driver = webdriver.Chrome(executable_path=driver_path)
+driver_path = '/app/.chromedriver/bin/chromedriver'
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+driver = webdriver.Chrome(options=options, executable_path=driver_path)
 
 driver.get('https://search.rakuten.co.jp/search/mall/%E3%83%80%E3%82%A4%E3%82%A8%E3%83%83%E3%83%88/')
 
