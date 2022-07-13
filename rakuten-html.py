da71dac3035fcf143777919e2d065d79e942b551
kuwamoto6 <y_kuwamoto@webkikaku.co.jp>
2022-07-13 16:01:45 +0900

    “commit”

--- rakuten-html.py
+++ rakuten-html.py
@@ -7,3 +7,5 @@ driver = webdriver.Chrome(executable_path=driver_path)
 driver.get('https://search.rakuten.co.jp/search/mall/%E3%83%80%E3%82%A4%E3%82%A8%E3%83%83%E3%83%88/')
 
 html = driver.page_source
+
+print(html)
