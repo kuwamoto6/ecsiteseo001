8b4193929132f0d6e1036c242739b2d5fd9b70fd
kuwamoto6 <y_kuwamoto@webkikaku.co.jp>
2022-07-14 15:40:02 +0900

    Update rakuten-html.py

--- rakuten-html.py
+++ rakuten-html.py
@@ -7,6 +7,7 @@ driver_path = '/app/.chromedriver/bin/chromedriver'
 chrome_service = fs.Service(executable_path=driver_path)
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
+options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(service=chrome_service, options=options)
 
 driver.get('https://search.rakuten.co.jp/search/mall/%E3%83%80%E3%82%A4%E3%82%A8%E3%83%83%E3%83%88/')
