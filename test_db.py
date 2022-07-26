83290dcce933ee6ebe2255a4911a7aac2fec0d0f
kuwamoto6 <y_kuwamoto@webkikaku.co.jp>
2022-07-26 17:27:37 +0900

    Update test_db.py

--- test_db.py
+++ test_db.py
@@ -1,12 +1,13 @@
-import os
 import psycopg2
 
-DATABASE_URL = os.environ.get('postgres://abiiwxvsfsgqub:f73007982168170d1171b2f3bc07a95e5d7dc50df1e321168badcd8f86ceb956@ec2-34-233-115-14.compute-1.amazonaws.com:5432/de5m80kjjkv3cj')
+db = PostgreConnect (
+        'ec2-34-233-115-14.compute-1.amazonaws.com',#Host
+        'de5m80kjjkv3cj',#Database
+        'public',#schema
+        'abiiwxvsfsgqub',#User
+        'f73007982168170d1171b2f3bc07a95e5d7dc50df1e321168badcd8f86ceb956',#Password
+        port=5432#Port
+        )
 
-day = '2022/07/22'
-rakuten = 'テスト'
-
-with psycopg2.connect(DATABASE_URL) as conn:
-    with conn.cursor() as curs:
-        curs.execute(
-            "INSERT INTO aequalis(day, rakuten) VALUES(%s, %s)", (day, rakuten))
+#指定テーブルへの列追加
+db.add_column('aequalis',['day 2022/07/26','rakuten test'])
