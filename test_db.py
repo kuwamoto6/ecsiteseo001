00eb8382de079a43dff0525b35ccd7e5bc3e38e8
kuwamoto6 <y_kuwamoto@webkikaku.co.jp>
2022-07-26 18:28:52 +0900

    Update test_db.py

--- test_db.py
+++ test_db.py
@@ -9,7 +9,10 @@ cur = conn.cursor()  # カーサー
 cur.execute("select version()") # クエリの実行
 print(cur.fetchone()) 
 
+cur.execute("INSERT INTO aequalis (day) VALUES ('2022/07/26')")
 cur.execute("INSERT INTO aequalis (rakuten) VALUES ('テスト')")
+cur.execute("INSERT INTO aequalis (amazon) VALUES ('テスト')")
+cur.execute("INSERT INTO aequalis (yahoo) VALUES ('テスト')")
 conn.commit()
 
 # コネクション等は閉じる。
