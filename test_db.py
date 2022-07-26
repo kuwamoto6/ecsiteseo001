165adc5636e85037d37cf84024579ff48bcb7376
kuwamoto6 <y_kuwamoto@webkikaku.co.jp>
2022-07-26 17:46:35 +0900

    Update test_db.py

--- test_db.py
+++ test_db.py
@@ -1,13 +1,13 @@
 import psycopg2
 
-db = PostgreConnect (
-        'ec2-34-233-115-14.compute-1.amazonaws.com',#Host
-        'de5m80kjjkv3cj',#Database
-        'public',#schema
-        'abiiwxvsfsgqub',#User
-        'f73007982168170d1171b2f3bc07a95e5d7dc50df1e321168badcd8f86ceb956',#Password
-        port=5432#Port
-        )
+connector =  psycopg2.connect('postgres://abiiwxvsfsgqub:f73007982168170d1171b2f3bc07a95e5d7dc50df1e321168badcd8f86ceb956@ec2-34-233-115-14.compute-1.amazonaws.com:5432/de5m80kjjkv3cj'.format( 
+                user='abiiwxvsfsgqub',        #ユーザ
+                password='f73007982168170d1171b2f3bc07a95e5d7dc50df1e321168badcd8f86ceb956',  #パスワード
+                host='ec2-34-233-115-14.compute-1.amazonaws.com',       #ホスト名
+                port='5432',            #ポート
+                dbname='de5m80kjjkv3cj'))    #データベース名
+
+cursor = connector.cursor()
 
 #指定テーブルへの列追加
-db.add_column('aequalis',['day 2022/07/26','rakuten test'])
+cursor.add_column('aequalis',['day 2022/07/26','rakuten test'])
